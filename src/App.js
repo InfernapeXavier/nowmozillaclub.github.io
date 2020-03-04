0c7ad4bac1d4f36b00ceda0c0979578a36732dab
InfernapeXavier <itsgood37@ymail.com>
2020-03-04 15:20:53 -0500

    Update App.js
    - Dumbass Autoformatter

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ class App extends Component {
     return (
       <div className="App">
         <Helmet>
-          <body className="has-navbar-fixed-top"/>
-          <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet"/>
-          <script defer src="https://use.fontawesome.com/releases/v5.6.1/js/all.js" integrity="sha384-R5JkiUweZpJjELPWqttAYmYM1P3SNEJRM6ecTQF05pFFtxmCO+Y1CiUhvuDzgSVZ" crossorigin="anonymous"/>
+          <body className="has-navbar-fixed-top" />
+          <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" />
+          <script defer src="https://use.fontawesome.com/releases/v5.6.1/js/all.js" integrity="sha384-R5JkiUweZpJjELPWqttAYmYM1P3SNEJRM6ecTQF05pFFtxmCO+Y1CiUhvuDzgSVZ" crossorigin="anonymous" />
         </Helmet>
-        <Navbar/>
-        <Content/>
-        <Footer/>
+        <Navbar />
+        <Content />
+        <Footer />
       </div>
     );
   }
